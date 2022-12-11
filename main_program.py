33c8c13bf54abb7bd540bf4810335bdc668fa3de
WhiteAlucard <claudio.oulego@gmail.com>
2022-12-11 20:18:40 -0300

    base functionality on the app

--- main_program.py
+++ main_program.py
@@ -1,8 +1,27 @@
 import pandas as pd
 import PySimpleGUI as sg
+import os
 
-event, values = sg.Window('Get filename example', [
+working_directory = os.getcwd()
+layout = [
  [sg.Text('Filename')],
- [sg.Input(), sg.FileBrowse()], 
- [sg.OK(), sg.Cancel()]
- [sg.Button("Procesar")] ]).read(close=True)
+ [sg.Input(key="-FILE_PATH-"), sg.FileBrowse(initial_folder=working_directory)], 
+ [sg.Button("Procesar")],
+ [sg.Exit()], 
+ [sg.Text(key='-OUT-LOG-')]]
+
+window = sg.Window("Procesamiento de descargas", layout)
+
+while True:
+    event, values = window.read()
+    if event in (sg.WIN_CLOSED, 'Exit'):
+        break
+
+    elif event == "Procesar":
+            file_path = values["-FILE_PATH-"]
+            excel_file = pd.read_excel(file_path)
+            print(excel_file)
+            window['-OUT-LOG-'].update("Se cargó el archivo necesario")
+            
+
+window.close()
