9c0a036c619dd8e954c594fef7506a487f29d532
WhiteAlucard <claudio.oulego@gmail.com>
2022-12-11 20:39:51 -0300

    main process working fine

--- functions.py
+++ functions.py
@@ -2,9 +2,9 @@ import pandas as pd
 def load_data(file_name):
     return pd.read_excel("Libro4.xlsx")
 
-def mask_dataframe_single_date(date):
-    mask = (df['Fecha Mvto'] == date) 
-    return df.loc[mask]
+def mask_dataframe_single_date(dataframe,date):
+    mask = (dataframe['Fecha Mvto'] == date) 
+    return dataframe.loc[mask]
 
 def cantidad_de_repartos(df):
     repartos = df['Número']
@@ -14,7 +14,7 @@ def dataframe_cleaning(df):
     df_less_cols=df[['Fecha Mvto','Artículo','Descripción Artículo','Bultos','Unids']]
     articulos = df_less_cols[['Artículo','Descripción Artículo','Fecha Mvto']]
     articulos_unicos = articulos.drop_duplicates(subset="Artículo")
-    return df_less_cols,articulos_unicos 
+    return df_less_cols,articulos_unicos
 
 def suma_y_merge(df_datos,df_articulos):
     df_suma = df_datos.groupby('Artículo').sum()
--- main_program.py
+++ main_program.py
@@ -2,13 +2,30 @@ import pandas as pd
 import PySimpleGUI as sg
 import os
 
+import functions 
+
 working_directory = os.getcwd()
-layout = [
+left_col = [
  [sg.Text('Filename')],
  [sg.Input(key="-FILE_PATH-"), sg.FileBrowse(initial_folder=working_directory)], 
  [sg.Button("Procesar")],
- [sg.Exit()], 
  [sg.Text(key='-OUT-LOG-')]]
+
+
+def Proceso(fecha, archivo):
+    df_filtrado_por_fecha = functions.mask_dataframe_single_date(archivo, fecha)
+    repartos = functions.cantidad_de_repartos(df_filtrado_por_fecha)
+    df_menos_columnas, articulos_unicos = functions.dataframe_cleaning(df_filtrado_por_fecha)
+    df_final = functions.suma_y_merge(df_menos_columnas, articulos_unicos)
+    return df_final,repartos
+
+
+
+right_col = [[ sg.Text('Fecha DES', size=(12,1))],
+            [ sg.Input(key='-FECHA-', size=(12,1))], [sg.Exit()]]
+
+
+layout = [[sg.Column(left_col, element_justification='left'),sg.Column(right_col, element_justification='left') ] ]
 
 window = sg.Window("Procesamiento de descargas", layout)
 
@@ -20,8 +37,9 @@ while True:
     elif event == "Procesar":
             file_path = values["-FILE_PATH-"]
             excel_file = pd.read_excel(file_path)
-            print(excel_file)
             window['-OUT-LOG-'].update("Se cargó el archivo necesario")
-            
-
+            fecha = values['-FECHA-']
+            df_final,repartos = Proceso(fecha,excel_file)
+            print(df_final)
+            print(repartos)
 window.close()
